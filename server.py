1c1c4becab9b9cfcd7aca256cbbe611b91bad33d
사용자 <user@example.com>
2026-04-19 04:03:37 +0900

    1

--- server.py
+++ server.py
@@ -240,13 +240,12 @@ def exchange_loop():
                     macro._DIRECTION_FUNCS[macro.high_count_direction]()
                     time.sleep(1)
 
-            if time.time() - _last_type_string_time >= 12:
+            if time.time() - _last_type_string_time >= 8:
                 _ad_formats = [
-                    f"\\f2 헤이 {macro.adena_per_pickup} \\f={total_count}방!",
-                    f"\\f2 {total_count}방 가능 \\f=한방에 {macro.adena_per_pickup}아데나!",
-                    f"\\f2 헤이 200 \\f= 6방 1200",
-                    f"\\f2 {total_count}방 팝니다~ {macro.adena_per_pickup}",
-                    f"\\f2 {macro.adena_per_pickup}에 {total_count}방 ㄱㄱ",
+                    f"\\f2헤이 {macro.adena_per_pickup} \\f={total_count}방 !",
+                    f"\\f2{total_count}방 가능 \\f=한방에 {macro.adena_per_pickup}아데나!",
+                    f"\\f2헤이 {macro.adena_per_pickup} \\f= 6방 {macro.adena_per_pickup * 6}",
+                    f"\\f2{macro.adena_per_pickup}에 {total_count}방 ㄱㄱ",
                 ]
                 macro.arduino_type_string(random.choice(_ad_formats))
                 _last_type_string_time = time.time()
